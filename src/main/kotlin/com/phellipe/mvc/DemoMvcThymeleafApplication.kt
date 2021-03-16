6dab7170e8b2ab0b4ec0466971ed2bbc995c2f21
Phellipe Rodrigues <phelliperodrigues.dev@gmail.com>
2021-03-15 21:21:44 -0300

    Initial Commit

---
+++ src/main/kotlin/com/phellipe/mvc/DemoMvcThymeleafApplication.kt
@@ -0,0 +1,11 @@
+package com.phellipe.mvc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoMvcThymeleafApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoMvcThymeleafApplication>(*args)
+}
---
+++ src/test/kotlin/com/phellipe/mvc/DemoMvcThymeleafApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.phellipe.mvc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoMvcThymeleafApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
